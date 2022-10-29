9c8f880af2420d81f77facc5aea8e27a42e514e1
adigulalkari <adigulalkari825@gmail.com>
2022-10-29 13:13:15 +0530

    Fixed Merge Issue: Added TodoItem Model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 """ Module defining all database models"""
 
 # Imports
+from xmlrpc.client import Boolean
 from sqlalchemy import Column, Integer, String, TIMESTAMP 
 
 from app.db import Base
@@ -8,20 +9,14 @@ from app.db import Base
 
 # Table models
 class TodoItem(Base):
-    """
-    TodoItem table model that stores the following data - 
-    - id: Unique id for each todo item
-    - title: Title of the todo item
-    - description: Description of the todo item
-    - created_at: Timestamp of when the todo item was created
-    - updated_at: Timestamp of when the todo item was last updated
-    - completed: Boolean value indicating if the todo item is completed or not
-    """
-    
     __tablename__ = "todo_items"
     
     id = Column(Integer, primary_key=True, index=True)
-
+    title = Column(String)
+    description = Column(String(length = 300))
+    created_at = Column(TIMESTAMP)
+    updated_at = Column(TIMESTAMP)
+    completed = Column(Boolean)
 
     def __repr__(self):
         """

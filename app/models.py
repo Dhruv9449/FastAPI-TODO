8211965fb7dcdea224bc3c9918a3e80d3bc84f39
adigulalkari <adigulalkari825@gmail.com>
2022-10-29 21:43:42 +0530

    Changed default val of TIMESTAMP and added  __repr__ func

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 """ Module defining all database models"""
 
 # Imports
-from xmlrpc.client import Boolean
-from sqlalchemy import Column, Integer, String, TIMESTAMP 
+from email.policy import default
+from enum import unique
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Boolean, func
 
 from app.db import Base
 
@@ -12,15 +13,11 @@ class TodoItem(Base):
     __tablename__ = "todo_items"
     
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String(length = 300))
-    created_at = Column(TIMESTAMP)
-    updated_at = Column(TIMESTAMP)
-    completed = Column(Boolean)
+    title = Column(String, unique=False)
+    description = Column(String(length = 300), unique=False)
+    created_at = Column(TIMESTAMP, default = func.now())
+    updated_at = Column(TIMESTAMP, default = func.now())
+    completed = Column(Boolean, unique= False)
 
     def __repr__(self):
-        """
-        Function to return the string representation of the TodoItem object.
-        Should be of the form <id>. <title>"
-        """
-        pass
+        return f'id={self.id} title={self.title}'
